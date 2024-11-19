2a5463ea9372349e91bc2875cd559c29ec67142b
CursedOn3 <rishavkc021365@nec.edu.np>
2024-11-19 21:09:53 +0545

    restarting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-// import Login from './pages/Login'
-// import Register from './pages/Register'
-import ContactUs from './pages/ContactUs'
+
 
 const App = () => {
   return (
     <div>
-      <ContactUs />
+     ad
     </div>
   )
 }
