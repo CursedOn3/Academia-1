03d3f46ef1b129970076c69e19084e1f7950b135
CursedOn3 <rishavkc021365@nec.edu.np>
2024-11-20 13:31:52 +0545

    something wrong with db connectivity

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './database/db.js';
+
+dotenv.config();
+
+//calling db connection
+// connectDB();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen (port, () => {
   console.log(`Server is running on port ${port}`);
-}
+});
