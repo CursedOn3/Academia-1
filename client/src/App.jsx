16866540b7d7e80ba7c009eb22872d86f2507e6d
CursedOn3 <rishavkc021365@nec.edu.np>
2024-11-21 16:02:22 +0545

    course progress, filter, search page and search result page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,95 @@
-import React from 'react'
-import Login from './pages/Login'
-import Navbar from './components/Navbar'
-import HeroSection from './pages/student/HeroSection'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import MainLayout from './layout/MainLayout'
-import Course from './pages/student/Course'
-import MyLearning from './pages/student/MyLearning'
-import Profile from './pages/student/Profile'
-import Sidebar from './pages/admin/Sidebar'
-import Dashboard from './pages/admin/Dashboard'
-import CourseTable from './pages/admin/course/CourseTable'
-import AddCourse from './pages/admin/course/AddCourse'
-import EditCourse from './pages/admin/course/EditCourse'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import HeroSection from "./pages/student/HeroSection";
+import MainLayout from "./layout/MainLayout";
+import Courses from "./pages/student/Courses";
+import MyLearning from "./pages/student/MyLearning";
+import Profile from "./pages/student/Profile";
+import Sidebar from "./pages/admin/Sidebar";
+import Dashboard from "./pages/admin/Dashboard";
+import CourseTable from "./pages/admin/course/CourseTable";
+import AddCourse from "./pages/admin/course/AddCourse";
+import EditCourse from "./pages/admin/course/EditCourse";
+import CreateLecture from "./pages/admin/lecture/CreateLecture";
+import EditLecture from "./pages/admin/lecture/EditLecture";
+import CourseDetail from "./pages/student/CourseDetail";
+import CourseProgress from "./pages/student/CourseProgress";
+import SearchPage from "./pages/student/SearchPage";
+import {
+  AdminRoute,
+  AuthenticatedUser,
+  ProtectedRoute,
+} from "./components/ProtectedRoutes";
+import PurchaseCourseProtectedRoute from "./components/PurchaseCourseProtectedRoute";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const appRouter = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
       {
-        path: '/',
-        element: <>
-        <HeroSection />
-        <Course />
-        </>
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            <Courses />
+          </>
+        ),
       },
       {
-        path: 'login',
-        element: <Login />
+        path: "login",
+        element: (
+          <AuthenticatedUser>
+            <Login />
+          </AuthenticatedUser>
+        ),
       },
       {
-        path: 'profile',
-        element: <Profile />
+        path: "my-learning",
+        element: (
+          <ProtectedRoute>
+            <MyLearning />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course/search",
+        element: (
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course-detail/:courseId",
+        element: (
+          <ProtectedRoute>
+            <CourseDetail />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "course-progress/:courseId",
+        element: (
+          <ProtectedRoute>
+            <PurchaseCourseProtectedRoute>
+            <CourseProgress />
+            </PurchaseCourseProtectedRoute>
+          </ProtectedRoute>
+        ),
       },
 
-      //Admin Routes
+      // admin routes start from here
       {
         path: "admin",
         element: (
@@ -66,19 +121,21 @@ const appRouter = createBrowserRouter([
           {
             path: "course/:courseId/lecture/:lectureId",
             element: <EditLecture />,
-          }
+          },
         ],
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 
-const App = () => {
+function App() {
   return (
     <main>
-      <RouterProvider router={appRouter}/> 
+      <ThemeProvider>
+      <RouterProvider router={appRouter} />
+      </ThemeProvider>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
