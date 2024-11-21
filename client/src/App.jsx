49dabede2c717261da18739fb05e74d0158075a5
CursedOn3 <rishavkc021365@nec.edu.np>
2024-11-21 11:06:25 +0545

    made some admin pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import MainLayout from './layout/MainLayout'
 import Course from './pages/student/Course'
 import MyLearning from './pages/student/MyLearning'
 import Profile from './pages/student/Profile'
+import Sidebar from './pages/admin/Sidebar'
+import Dashboard from './pages/admin/Dashboard'
 
 const appRouter = createBrowserRouter([
   {
@@ -27,7 +29,43 @@ const appRouter = createBrowserRouter([
       {
         path: 'profile',
         element: <Profile />
-      }
+      },
+
+      //Admin Routes
+      {
+        path: "admin",
+        element: (
+          <AdminRoute>
+            <Sidebar />
+          </AdminRoute>
+        ),
+        children: [
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "course",
+            element: <CourseTable />,
+          },
+          {
+            path: "course/create",
+            element: <AddCourse />,
+          },
+          {
+            path: "course/:courseId",
+            element: <EditCourse />,
+          },
+          {
+            path: "course/:courseId/lecture",
+            element: <CreateLecture />,
+          },
+          {
+            path: "course/:courseId/lecture/:lectureId",
+            element: <EditLecture />,
+          }
+        ],
+      },
     ]
   }
 ])
