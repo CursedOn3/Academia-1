c31fb3da32b733e12abb2b61fa9693533a607595
CursedOn3 <rishavkc021365@nec.edu.np>
2024-11-22 00:30:19 +0545

    fixed error in server

--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import isAuthenticated from "../middleware/isAuthenticated.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getLectureById, getPublishedCourse, removeLecture, searchCourse, togglePublishCourse } from "../controllers/course.controller.js";
 import upload from "../utils/multer.js";
 const router = express.Router();
