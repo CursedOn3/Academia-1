7300f5a2c755aaf2e75baba8d0ac9569f6b6b346
CursedOn3 <rishavkc021365@nec.edu.np>
2024-11-21 15:56:21 +0545

    course detail page

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ import connectDB from './database/db.js';
 import userRoute from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import mediaRoute from './routes/media.route.js';
+import courseRoute from './routes/course.route.js';
 
 dotenv.config();
 
+//DB connection here
 connectDB();
 
 const app = express();
@@ -16,12 +19,15 @@ const port = process.env.PORT || 3000;
 //default middleware
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
 }));
 
 //APIs
+app.use("/api/v1/media", mediaRoute);
+app.use("/api/v1/course", courseRoute);
 app.use('/api/v1/user', userRoute);
 
 "http://localhost:5000/api/v1/user/register"
