abb592499e84f88b621a32b78bd9bdda3027ee91
CursedOn3 <rishavkc021365@nec.edu.np>
2024-11-22 00:30:19 +0545

    still error with DB maybe

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import { register, login } from '../controllers/user.controller.js';
-import { get } from 'mongoose';
-import isAuthenticated from '../middleware/isAuthenticated.js';
-import upload from '../utils/multer.js';
+import express from "express";
+import { getUserProfile, login, logout, register, updateProfile } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../utils/multer.js";
 
 const router = express.Router();
 
-router.route('/register').post(register);
-router.route('/login').post(login);
-router.route('/profile').get(isAuthenticated, getUserProfile);
-router.route('/logout').get(isAuthenticated, logout);
-router.route('/profile/update').put(isAuthenticated, upload.single("profilePhoto"), updateUser);
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/profile").get(isAuthenticated, getUserProfile);
+router.route("/profile/update").put(isAuthenticated, upload.single("profilePhoto"), updateProfile);
 
 export default router;
